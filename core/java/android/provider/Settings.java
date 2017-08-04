2c10fb92786dba0999f174ef3d834d40bf71183f
LorDClockaN <lordclockan@gmail.com>
2017-08-04 15:43:57 +0200

    base: Sensor block per-package switch (1/2)
    Change-Id: I3a5cb8820ca6815c7140c25368e525faa2ecc5e6
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5676,6 +5676,12 @@ public final class Settings {
         public static final String SETTINGS_SHOW_SUGGESTIONS = "settings_show_suggestions";
 
         /**
+         * Sensor block per-package
+         * @hide
+         */
+        public static final String SENSOR_BLOCK = "sensor_block";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
