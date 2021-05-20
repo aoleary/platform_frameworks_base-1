5bcf19cf809ee5682184e7c2326e821303e27e38
Jorim Jaggi <jjaggi@google.com>
2021-05-20 18:35:30 +0200

    Fix race condition between lockNow() and updateLockscreenTimeout
    If updateLockscreenTimeout gets called before the Runnable queued
    from lockNow gets executed, lockNow request will be ignored. Fix
    this by not clearing out the runnable if it's pending lock request.
    
    Test: Switch user, ensure lockscreen comes up
    Bug: 161149543
    Change-Id: Ie486396fd7328edf8ca0912df92524bb82a1fb7f
    (cherry picked from commit 875fa991aac0f3bbd5c66327408ceae60a24a6b3)
    Merged-In: Ie486396fd7328edf8ca0912df92524bb82a1fb7f
    (cherry picked from commit 1d31270fd256b50c32f7b5f47ca61d1b96c9b4a7)

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -898,6 +898,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     };
     private final List<DeviceKeyHandler> mDeviceKeyHandlers = new ArrayList<>();
+    private boolean mLockNowPending = false;
 
     private static final int MSG_ENABLE_POINTER_LOCATION = 1;
     private static final int MSG_DISABLE_POINTER_LOCATION = 2;
@@ -8535,6 +8536,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     mKeyguardDelegate.doKeyguardTimeout(options);
                 }
                 mLockScreenTimerActive = false;
+                mLockNowPending = false;
                 options = null;
             }
         }
@@ -8544,7 +8546,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     }
 
-    ScreenLockTimeout mScreenLockTimeout = new ScreenLockTimeout();
+    final ScreenLockTimeout mScreenLockTimeout = new ScreenLockTimeout();
 
     @Override
     public void lockNow(Bundle options) {
@@ -8556,10 +8558,17 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             mScreenLockTimeout.setLockOptions(options);
         }
         mHandler.post(mScreenLockTimeout);
+        synchronized (mScreenLockTimeout) {
+            mLockNowPending = true;
+        }
     }
 
     private void updateLockScreenTimeout() {
         synchronized (mScreenLockTimeout) {
+            if (mLockNowPending) {
+                Log.w(TAG, "lockNow pending, ignore updating lockscreen timeout");
+                return;
+            }
             boolean enable = (mAllowLockscreenWhenOn && mAwake &&
                     mKeyguardDelegate != null && mKeyguardDelegate.isSecure(mCurrentUserId));
             if (mLockScreenTimerActive != enable) {
